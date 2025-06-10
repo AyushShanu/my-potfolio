354f41ca965c19922e95f7c50fb8259d109d80c8
AyushShanu <dhuliyaayush1601@gmail.com>
2025-06-10 07:09:54 +0530

    Fix: null check added to mesh.current

--- components/3d/geometric-particles.tsx
+++ components/3d/geometric-particles.tsx
@@ -1,47 +1,47 @@
-    "use client";
+"use client";
 
-    import { useRef, useMemo } from "react";
-    import { useFrame } from "@react-three/fiber";
-    import * as THREE from "three";
-    import { useTheme } from "next-themes";
+import { useRef, useMemo } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { useTheme } from "next-themes";
 
-    type GeometricParticlesProps = {
+type GeometricParticlesProps = {
     count?: number;
     color?: string;
     size?: number;
     speed?: number;
-    };
+};
 
-    export function GeometricParticles({
+export function GeometricParticles({
     count = 100,
     color = "#ffffff",
     size = 0.2,
     speed = 0.2,
-    }: GeometricParticlesProps) {
+}: GeometricParticlesProps) {
     const mesh = useRef<THREE.InstancedMesh>(null);
     const { theme } = useTheme();
-    
+
     // Create particles data
     const particles = useMemo(() => {
         const temp = [];
         for (let i = 0; i < count; i++) {
-        const position = new THREE.Vector3(
-            (Math.random() - 0.5) * 10,
-            (Math.random() - 0.5) * 10,
-            (Math.random() - 0.5) * 10
-        );
-        const rotation = new THREE.Euler(
-            Math.random() * Math.PI,
-            Math.random() * Math.PI,
-            Math.random() * Math.PI
-        );
-        const scale = Math.random() * 0.5 + 0.5;
-        const rotationSpeed = {
-            x: (Math.random() - 0.5) * 0.01 * speed,
-            y: (Math.random() - 0.5) * 0.01 * speed,
-            z: (Math.random() - 0.5) * 0.01 * speed,
-        };
-        temp.push({ position, rotation, scale, rotationSpeed });
+            const position = new THREE.Vector3(
+                (Math.random() - 0.5) * 10,
+                (Math.random() - 0.5) * 10,
+                (Math.random() - 0.5) * 10
+            );
+            const rotation = new THREE.Euler(
+                Math.random() * Math.PI,
+                Math.random() * Math.PI,
+                Math.random() * Math.PI
+            );
+            const scale = Math.random() * 0.5 + 0.5;
+            const rotationSpeed = {
+                x: (Math.random() - 0.5) * 0.01 * speed,
+                y: (Math.random() - 0.5) * 0.01 * speed,
+                z: (Math.random() - 0.5) * 0.01 * speed,
+            };
+            temp.push({ position, rotation, scale, rotationSpeed });
         }
         return temp;
     }, [count, speed]);
@@ -56,17 +56,19 @@
         if (!mesh.current) return;
 
         particles.forEach((particle, i) => {
-        particle.rotation.x += particle.rotationSpeed.x;
-        particle.rotation.y += particle.rotationSpeed.y;
-        particle.rotation.z += particle.rotationSpeed.z;
+            particle.rotation.x += particle.rotationSpeed.x;
+            particle.rotation.y += particle.rotationSpeed.y;
+            particle.rotation.z += particle.rotationSpeed.z;
 
-        const matrix = new THREE.Matrix4();
-        matrix.compose(
-            particle.position,
-            new THREE.Quaternion().setFromEuler(particle.rotation),
-            new THREE.Vector3(particle.scale, particle.scale, particle.scale)
-        );
-        mesh.current.setMatrixAt(i, matrix);
+            const matrix = new THREE.Matrix4();
+            matrix.compose(
+                particle.position,
+                new THREE.Quaternion().setFromEuler(particle.rotation),
+                new THREE.Vector3(particle.scale, particle.scale, particle.scale)
+            );
+            if (mesh.current) {
+                mesh.current.setMatrixAt(i, matrix);
+            }
         });
 
         mesh.current.instanceMatrix.needsUpdate = true;
@@ -76,13 +78,13 @@
 
     return (
         <instancedMesh ref={mesh} args={[geometry, undefined, count]}>
-        <meshStandardMaterial
-            color={color || particleColor}
-            roughness={0.5}
-            metalness={0.2}
-            transparent
-            opacity={0.7}
-        />
+            <meshStandardMaterial
+                color={color || particleColor}
+                roughness={0.5}
+                metalness={0.2}
+                transparent
+                opacity={0.7}
+            />
         </instancedMesh>
     );
-    }
+}
