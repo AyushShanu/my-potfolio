bed13d0741ffc6467e6abea11f56c0415b5ea9f9
AyushShanu <dhuliyaayush1601@gmail.com>
2025-06-12 09:42:12 +0530

    fixing new error that comes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    // Prevent build failure on dynamic requires from libraries like Supabase
+    config.module.exprContextCritical = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
